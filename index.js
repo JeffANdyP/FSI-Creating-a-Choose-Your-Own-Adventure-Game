df9faf19d0c562cf7a1f3ffcab83cffc2d4607c0
Andys-in <andyjeffersonperez@gmail.com>
2022-09-19 13:35:56 -0400

    my changes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-//Your Code here
+
+
+let response = window.prompt("what did you do that was creative")
+console.log (response);
+
+if(response === "answer"){
+    //we run logic for any type of reponse.
+} else if (response === "answer")
